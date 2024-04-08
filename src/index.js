d3ba3deea87209acb81b1b1509a2b542b607131b
FedTaras <113994712+FedTaras@users.noreply.github.com>
2024-04-08 12:04:10 +0300

    Updated

--- src/index.js
+++ src/index.js
@@ -3,12 +3,43 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_Z8rk7r6o2o5bKYW0PZDYGLMnKNHaWQwj31xP2mvX7g6bDmnP0hnfnXCspDC5loHY';
 
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-import { select, textError, textLoading, catInfo } from './const';
+// import { fetchBreeds, fetchCatByBreed } from './cat-api';
+// import { select, textError, textLoading, catInfo } from './const';
+
+const HEADER = 'https://api.thecatapi.com/v1/';
+const select = document.querySelector('.breed-select');
+const textLoading = document.querySelector('.loader');
+const textError = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
+
+function fetchBreeds() {
+  return fetch(`${HEADER}breeds?x-api-key`).then(resp => {
+    if (!resp.ok) {
+      console.log(resp.statusText);
+      throw new Error(resp.statusText);
+    }
+    textLoading.hidden = true;
+    select.hidden = false;
+    return resp.json();
+  });
+}
+
+function fetchCatByBreed(breedId) {
+  return fetch(`${HEADER}images/search/?breed_ids=${breedId}`).then(resp => {
+    if (!resp.ok) {
+      console.log(resp.statusText);
+      throw new Error(resp.statusText);
+    }
+    textLoading.hidden = true;
+    select.hidden = false;
+    return resp.json();
+  });
+}
 
 select.hidden = true;
 textError.hidden = true;
 textLoading.hidden = false;
+// catInfo.style.margin = '10px';
 
 fetchBreeds()
   .then(data => {
